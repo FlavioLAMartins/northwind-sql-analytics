e6df5fe7e645281cb806212fee797c0db0363083
FlavioLAM <flavio_martins11@hotmail.com>
2026-03-29 03:13:11 -0300

    Add UK customers with payments over 1000 analysis

---
+++ queries/05_uk_customers_over_1000.sql
@@ -0,0 +1,21 @@
+-- Objective: Identify UK customers with high total payments
+-- Criteria: Total payments greater than 1000 (net revenue)
+-- Filter: Country = UK
+
+CREATE VIEW vw_uk_customers_with_payments_greater_than_1000 AS
+SELECT 
+    c.customer_id,
+    c.company_name,
+    c.country,
+    SUM((od.unit_price * od.quantity) * (1 - od.discount)) AS payments
+FROM customers c 
+INNER JOIN orders o 
+    ON c.customer_id = o.customer_id
+INNER JOIN order_details od 
+    ON o.order_id = od.order_id
+WHERE LOWER(c.country) = 'uk'
+GROUP BY 
+    c.customer_id,
+    c.company_name,
+    c.country
+HAVING SUM((od.unit_price * od.quantity) * (1 - od.discount)) > 1000;

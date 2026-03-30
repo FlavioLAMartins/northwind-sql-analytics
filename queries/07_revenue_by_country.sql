dcc260bcdb5b6df8637cd6b2c4ca1f508cbdce1a
FlavioLAM <flavio_martins11@hotmail.com>
2026-03-30 14:29:47 -0300

    Add revenue by country analysis based on shipping destination

---
+++ queries/07_revenue_by_country.sql
@@ -0,0 +1,11 @@
+-- Objective: Calculate total revenue by shipping country
+-- Metric: Net revenue grouped by order destination country
+
+CREATE VIEW vw_revenue_by_country AS
+SELECT 
+    o.ship_country,
+    SUM((od.unit_price * od.quantity) * (1 - od.discount)) AS total_revenue
+FROM orders o
+INNER JOIN order_details od 
+    ON o.order_id = od.order_id
+GROUP BY o.ship_country;
